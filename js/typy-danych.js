f3f0ba2440b23c7e795a5aaa6e382d1c9b15e7ac
Oleksandra <157833785+dulidawi@users.noreply.github.com>
2025-09-28 12:54:35 +0200

    123

---
+++ js/typy-danych.js
@@ -0,0 +1,56 @@
+// Boolean логічний тип, true or false
+// const age = 23
+// const isChild = age < 18
+// console.log(isChild);
+
+// Null - nothing
+// let data = null
+// console.log(data);
+
+// Underfind - нічого не було присвоєно
+// let name;
+// console.log(name);
+
+// Object
+// const user = {
+//     name: "Max",
+//     age: 12,
+// }
+
+// typeOf - перевірка типу данних
+// const message = "13"
+// console.log(typeof message);
+// console.log(typeof 100n);
+// console.log(typeof (100 + 300));
+
+// String()
+// const age = 11;
+// console.log(age); - number
+// console.log(String(age)); - string
+
+// Number()
+// const str = "1000"
+// const num = Number(str)
+// console.log(typeof num);
+
+// console.log(Number(true)); - 1
+// console.log(Number(false)); - 0
+// console.log(Number(null)); - 0
+// console.log(Number(undefined)); - NaN
+
+// Boolean
+// console.log(Boolean(-1)); - true
+// console.log(Boolean(0)); - false
+// console.log(Boolean(1)); - true
+// console.log(Boolean("Hello")); - true
+// console.log(Boolean("0")); - true
+// console.log(Boolean(" ")); - true
+// console.log(Boolean("")); - false
+
+// Преобразуются в false
+// console.log(Boolean(0));
+// console.log(Boolean(NaN));
+// console.log(Boolean(""));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// все інше - true
