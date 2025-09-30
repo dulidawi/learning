00295b57be38023834ac314e51c78bc2296262b9
Oleksandra <157833785+dulidawi@users.noreply.github.com>
2025-09-30 20:09:57 +0200

    123

---
+++ js/math.js
@@ -0,0 +1,56 @@
+// // степень
+// console.log(2 ** 10);
+
+// // залишок від ділення
+// console.log(13 % 5);
+// console.log(12 % 3);
+
+// // string -> number
+// console.log(typeof Number("123"));
+// console.log(typeof +"123");
+
+// console.log(+true); // 1
+// console.log(+false); // 0
+// console.log(+""); // 0
+// console.log(+" "); // 0
+// console.log(+null); // 0
+// console.log(+undefined); // NaN
+
+// console.log('3' + '5'); // 35
+// console.log(+'3' + +'5'); // 8
+
+// let count = 1;
+// count = count + 2 // 3
+// count += 2 // 3
+// console.log(count);
+
+// Оператори декремента
+
+// let count = 10;
+// count--
+// count--
+// count--
+// console.log(count);
+
+// Операток инкремента
+
+// let count = 23;
+// count++
+// count++
+// console.log(count);
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
