bb08f660804f3d1706bd7201a089dc0e4478368a
Oleksandra <157833785+dulidawi@users.noreply.github.com>
2025-10-03 16:52:07 +0200

    123

---
+++ js/functions.js
@@ -0,0 +1,63 @@
+// function declaration
+
+// function logHello() {
+//     console.log("Hello!");
+// }
+// logHello()
+
+// function logMessage(message, count) {
+//     for (let i = 0; i < count; i++) {
+//         console.log(message);
+//     }
+// }
+
+// logMessage("Hello!", 4);
+// logMessage("Bye!", 5);
+
+
+// const globalMessage = "Hello!"
+
+// function logMessage(message, count) {
+// const messageFormatted = `((${message}))`
+
+//     for (let i = 0; i < count; i++) {
+//         console.log(messageFormatted)
+//     }
+// }
+
+// logMessage(globalMessage, 3)
+
+// function sum(a, b) {
+//     return a + b
+// }
+
+// const result = sum(100, 400)
+// console.log(result);
+
+
+// function typeOfAge(age) {
+//     if (typeof age !== "number") {
+//         return "That's not a number!"
+//     }
+
+//     if (age < 1 || age > 125) {
+//         return "That's impossible!"
+//     }
+
+//     if (age < 18) {
+//         return "You're not an adult!"
+//     }
+
+//     return "You're adult!"
+// }
+
+// console.log(typeOfAge(15));
+
+// function secretName(name) {
+//     if (!name) return
+//     return `I know you ${name}!`
+// }
+
+// console.log(secretName("Max"));
+
+
