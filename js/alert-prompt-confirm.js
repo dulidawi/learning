8447a9ba84c62669b38701632dcad96773f31168
Oleksandra <157833785+dulidawi@users.noreply.github.com>
2025-10-03 15:25:37 +0200

    123

---
+++ js/alert-prompt-confirm.js
@@ -0,0 +1,50 @@
+// const user = prompt("Your age?", 18)
+// console.log(`Age: ${user}`);
+
+// if (user >= 18) {
+//     console.log("Welcome!");
+// } else {
+//     console.log("Goodbye!");
+// }
+
+// const userAge = Number(prompt("Your age?"))
+// if (userAge === 0) {
+//     console.log("That's impossible!");
+// } else {
+//     console.log(`Age: ${userAge}`);
+// }
+
+// SWITCH
+
+// const userAge = Number(prompt("Your age?"))
+// switch (userAge) {
+//     case 0: {
+//         console.log("That's impossible!");
+//         break
+//     }
+//     case 18: {
+//         console.log("Show me your ID!");
+//         break
+//     }
+//     default: {
+//         console.log(`Your age: ${userAge}`);
+//         }
+// }
+
+
+// const userAge = Number(prompt("Your age?"))
+// switch (true) {
+//     case userAge < 1: {
+//         console.log("That's impossible!");
+//         break
+//     }
+//     case userAge === 18: {
+//         console.log("Show me your ID");
+//         break
+//     }
+//     default: {
+//         console.log(`Age: ${userAge}`);
+//     }
+// }
+
+
---
+++ js/if-else.js
@@ -0,0 +1,64 @@
+// const year = 2024
+
+// if (year === 2025) {
+//     console.log("Woww!");
+// } else {
+//     console.log("Opss!");
+// }
+
+// const number = 4
+// if (number === 6) {
+//     console.log("Done!");
+// } else if (number === 4) {
+//     console.log("Right!");
+// } else {
+//     console.log("Opps!");
+// }
+
+// // Оператор АБО
+// false || false // false
+// false || true // true
+// true || false // true
+// true || true // true
+
+// const age = 18
+// const withParent = false
+
+// if (age >= 18 || withParent) {
+//     console.log("Welcome!");
+// } else {
+//     console.log("Goodbye!");
+// }
+
+   
+// Оператор І
+// console.log(true && false); // false
+// console.log(false && true); // false
+// console.log(false && false); // false
+// console.log(true && true); // true
+
+// const money = 5
+// const price = 4
+// const open = true
+
+// if (price < money && open) {
+//     console.log("Welcome!")    
+// } else {
+//     console.log('Goodbye!')    
+// }
+
+// const age = 12
+// const young = age < 18
+
+// if (young) {
+//     console.log("Show your ID!");    
+// } else {
+//     console.log("Welcome");    
+// }
+
+// console.log(!true); // false
+// console.log(!false); // true
+
+
+
+
