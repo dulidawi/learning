50226eca07d6db883d04a9fa70ce98dbca3282c1
Oleksandra <157833785+dulidawi@users.noreply.github.com>
2025-10-03 15:44:00 +0200

    123

--- js/while-for.js
+++ js/while-for.js
@@ -1,6 +1,35 @@
 // let count = 0
 // while (count < 10) {
 //     console.log(count);
-//     count++    
+//     count++
 // }
+
+// let count = 1
+// while (count < 10) {
+//     console.log(count);
+    
+//     if (count % 5 === 0) {
+//         console.log("Stop!");
+//         break
+//     }
+//     count++
+// }
+
+// for (let q = 1; q < 10; q++) {
+//     console.log(q);
+    
+//     if (q === 3) {
+//         console.log("Stop!");
+//         break
+//     }
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 === 0) {
+//         continue
+//     }
+//     console.log(i);
+// }
+
+
 
